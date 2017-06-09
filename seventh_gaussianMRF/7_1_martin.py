ee0f8548d9590babcf8e9bc83e118af671e50f9e
Lucas-Rapahel Müller <gitdev@LRM.photo>
2017-06-09 17:05:28 +0200

    up

--- seventh_gaussianMRF/7_1_martin.py
+++ seventh_gaussianMRF/7_1_martin.py
@@ -6,26 +6,59 @@
 
 
 from matplotlib import pyplot as plt
+import skimage
 from skimage import data
-from scipy import misc
+import scipy
+from scipy import misc, sparse
 import numpy as np
 
+
+# Bilateral Filter to prevent blurring
+
+
+def bilateral(ci, cj, alpha=-1, gamma=0.01):
+    a = (ci[0].astype(float) - cj[0].astype(float))
+    b = (ci[1].astype(float) - cj[1].astype(float))
+    c = (ci[2].astype(float) - cj[2].astype(float))
+
+    weight = np.sqrt(a ** 2 + b ** 2 + c ** 2)
+    return alpha * np.exp(-gamma * weight)
+
+def denoiseLoss(img, imgDenoised):
+    return np.linalg.norm(img.flatten() - imgDenoised.flatten())
+
+def denoise_performance(img, imgNoisy, imgDenoised):
+    perf = 1 - denoiseLoss(img, imgDenoised)/denoiseLoss(img, imgNoisy)
+    return perf
+
+
+# Parameters: alpha - weighting of neighboring pixels
+#             gamma - weighting of color difference of neighboring pixels
+#             s     - weighting of centered pixel
+#             sigma - weighting of the smoothing
+
+alpha = -1
+gamma = 0.01
+s = 0.5
+sigma = 1
+
 # A measurement of the world state. Here given by an astronaut
-img = data.astronaut()
-img = misc.imresize(img, [128, 128, 3])
+imgO = skimage.data.astronaut()
+
+# Resize the image
+imgO = scipy.misc.imresize(imgO, [100, 100, 3])
 
 # Add random noise
-blur = np.round(np.random.normal(0, 10, [img.shape[0], img.shape[1], img.shape[2]]))
+blur = np.round(np.random.normal(0, 25, [imgO.shape[0], imgO.shape[1], imgO.shape[2]]))
 
-imHelp = np.add(img, blur)
-np.clip(imHelp, 0, 255, out=imHelp)
-im = imHelp.astype(np.uint8)
+imgTemp = np.add(imgO, blur)
 
+img = np.clip(imgTemp, 0, 255, out=imgTemp).astype(np.uint8)
 
 # Decompose the image into red, green, and blue
-imgR = im[:, :, 0]
-imgG = im[:, :, 1]
-imgB = im[:, :, 2]
+imgR = img[:, :, 0]
+imgG = img[:, :, 1]
+imgB = img[:, :, 2]
 
 # Size of the MRF
 v = img.shape[0]
@@ -38,35 +71,38 @@ Dhr = np.zeros((v * h, v * h))
 Dvt = np.zeros((v * h, v * h))
 Dvb = np.zeros((v * h, v * h))
 
+# Create a dictionary that assigns every value on the diagonal of D to a pixel within the image img
+dic = {i * h + j: (i, j) for i in range(v) for j in range(h)}
+
 # Next assign the correct values to the matrices for 2 next horizontal and 2 next vertical neighbors
 for i in range(v * h):
     for j in range(v * h):
         if i == j:
-            Dhl[i, j] = 1
-            Dhr[i, j] = 1
+            Dhl[i, j] = s
+            Dhr[i, j] = s
             if i == j == 0:
-                Dhr[i, j + 1] = -1
+                Dhr[i, j + 1] = bilateral(img[dic[i]], img[dic[i + 1]], alpha, gamma)
             elif i == j == v * h - 1:
-                Dhl[i, j - 1] = -1
+                Dhl[i, j - 1] = bilateral(img[dic[i]], img[dic[i - 1]], alpha, gamma)
             elif (i + 1) % v == 0 and (j + 1) % h == 0:
-                Dhl[i, j - 1] = -1
+                Dhl[i, j - 1] = bilateral(img[dic[i]], img[dic[i - 1]], alpha, gamma)
                 Dhr[i, j + 1] = 0
             elif (i + 1) % v == 1 and (j + 1) % h == 1:
                 Dhl[i, j - 1] = 0
-                Dhr[i, j + 1] = -1
+                Dhr[i, j + 1] = bilateral(img[dic[i]], img[dic[i + 1]], alpha, gamma)
             else:
-                Dhl[i, j - 1] = -1
-                Dhr[i, j + 1] = -1
+                Dhl[i, j - 1] = bilateral(img[dic[i]], img[dic[i - 1]], alpha, gamma)
+                Dhr[i, j + 1] = bilateral(img[dic[i]], img[dic[i + 1]], alpha, gamma)
 
 for i in range(v * h):
     for j in range(v * h):
         if i == j:
-            Dvt[i, j] = 1
-            Dvb[i, j] = 1
+            Dvt[i, j] = s
+            Dvb[i, j] = s
             if j - h >= 0:
-                Dvt[i, j - h] = -1
+                Dvt[i, j - h] = bilateral(img[dic[i]], img[dic[i - h]], alpha, gamma)
             if j + h < v * h:
-                Dvb[i, j + h] = -1
+                Dvb[i, j + h] = bilateral(img[dic[i]], img[dic[i + h]], alpha, gamma)
 
 # np.dot(A, B) returns a matrix multiplication...
 Q = np.dot(np.transpose(Dhl), Dhl) + np.dot(np.transpose(Dhr), Dhr) + np.dot(np.transpose(Dvt), Dvt) + np.dot(
@@ -75,28 +111,32 @@ Q = np.dot(np.transpose(Dhl), Dhl) + np.dot(np.transpose(Dhr), Dhr) + np.dot(np.
 # Given the measurement, the system of linear equations can be solved to find the state that minimizes the MRF, and as
 # such, the state that the MRF holds for the most probable, given the measurement.
 
-sigma = 10
-A = np.identity(v * h) + sigma ** 2 * Q
+A = sparse.csr_matrix(np.identity(v * h) + sigma ** 2 * Q)
 
 imgR = np.reshape(imgR, (v * h, 1))
 imgG = np.reshape(imgG, (v * h, 1))
 imgB = np.reshape(imgB, (v * h, 1))
 
-optR = np.linalg.solve(A, imgR)
-optG = np.linalg.solve(A, imgG)
-optB = np.linalg.solve(A, imgB)
+optR = sparse.linalg.spsolve(A, imgR)
+optG = sparse.linalg.spsolve(A, imgG)
+optB = sparse.linalg.spsolve(A, imgB)
 
-imgR = np.reshape(imgR, (v, h))
-imgG = np.reshape(imgG, (v, h))
-imgB = np.reshape(imgB, (v, h))
+optR = np.reshape(optR, (v, h))
+optG = np.reshape(optG, (v, h))
+optB = np.reshape(optB, (v, h))
 
-imgOpt = np.stack((imgR, imgG, imgB), axis=2)
+imgOpt = np.stack((optR, optG, optB), axis=2).astype(np.uint8)
 
-plt.imshow(im)
-plt.show()
+performance = denoise_performance(imgO, img, imgOpt)
 
-plt.imshow(img)
-plt.show()
+f, axArr = plt.subplots(1, 3)
 
-plt.imshow(imgOpt)
-plt.show()
+axArr[0].imshow(imgO)
+axArr[1].imshow(img)
+axArr[2].imshow(imgOpt)
+
+axArr[0].set_title('Ground Truth')
+axArr[1].set_title('Noisy')
+axArr[2].set_title('Outcome \n performance=%f'%(performance))
+
+#plt.show()
